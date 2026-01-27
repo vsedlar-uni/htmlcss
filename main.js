96e0bfd3688bc09159dd3f132ec68d5072691caf
vladimir <vlado.sedlar@gmail.com>
2026-01-27 20:47:59 +0100

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+// Get elements
+const header = document.querySelector(".header");
+const main = document.querySelector(".main");
+const footer = document.querySelector(".footer");
+
+// Add event listener
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM is ready");
+});
