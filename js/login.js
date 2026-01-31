4b8f12def798576466fc958caf3caf6fa8baf8ff
vladimir <vlado.sedlar@gmail.com>
2026-01-31 17:45:16 +0100

    login: redirect to home, alert msg

--- js/login.js
+++ js/login.js
@@ -1,42 +1,42 @@
-const form = document.getElementById("loginForm")
+const form = document.getElementById("loginForm");
 
-const emailInput = document.getElementById("email")
-const passwordInput = document.getElementById("password")
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
 
 const emailError = document.getElementById("emailError");
-const passwordError = document.getElementById("passwordError")
+const passwordError = document.getElementById("passwordError");
 
 form.addEventListener("submit", function (e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    let isValid = true
+  let isValid = true;
 
-    emailError.textContent = ""
-    passwordError.textContent = ""
+  emailError.textContent = "";
+  passwordError.textContent = "";
 
-    const emailValue = emailInput.value.trim()
+  const emailValue = emailInput.value.trim();
 
-    if (emailValue === "") {
-        emailError.textContent = "Email is required.";
-        isValid = false
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue)) {
-        emailError.textContent = "Please enter a valid email address."
-        isValid = false
-    }
+  if (emailValue === "") {
+    emailError.textContent = "Email is required.";
+    isValid = false;
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue)) {
+    emailError.textContent = "Please enter a valid email address.";
+    isValid = false;
+  }
 
-    const passwordValue = passwordInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
 
-    if (passwordValue === "") {
-        passwordError.textContent = "Password is required.";
-        isValid = false
-    } else if (passwordValue.length < 6) {
-        passwordError.textContent = "Password must be at least 6 characters.";
-        isValid = false
-    }
+  if (passwordValue === "") {
+    passwordError.textContent = "Password is required.";
+    isValid = false;
+  }
 
-    // Submit if valid
-    if (isValid) {
-        alert("Success")
-        // form.submit()
-    }
-})
+  if (isValid) {
+    // TODO: store information in localstorage
+    // add redirect to home page
+    alert("You are logged in.");
+    window.location.href = "index.html";
+    // form.submit()
+  }
+});
+
